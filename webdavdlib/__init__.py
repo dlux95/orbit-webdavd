a186a89bc8d5899e2e967b0b30329b54e7aa0bfd
Daniel Laube <mail@dlaube.de>
2018-12-05 21:31:31 +0100

    Added some WebDAVResource Logic

--- webdavdlib/__init__.py
+++ webdavdlib/__init__.py
@@ -1,2 +1,16 @@
 
 
+class WebDAVResource(object):
+    def __init__(self):
+        self._properties = []
+        self._subresources = []
+
+    def addProperty(self, prop):
+        if any(p.__class__ == prop.__class__ for p in self._properties):
+            print("Duplicate property 1:", prop)
+        else
+            self._properties.append(prop)
+        return self
+
+    def addSubResource(self, subres):
+        self._subresources.append(subres)
