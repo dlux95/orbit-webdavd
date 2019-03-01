c859d60be5fbbba42623e52bb0d5f5e41354c614
Daniel Laube <mail@dlaube.de>
2019-03-01 11:52:31 +0100

    release preparation

--- orbit-webdavd.py
+++ orbit-webdavd.py
@@ -507,7 +507,7 @@ class WebDAVRequestHandler(BaseHTTPRequestHandler):
 
 if __name__ == "__main__":
     root_logger = logging.getLogger()
-    root_logger.setLevel("DEBUG")
+    root_logger.setLevel("INFO")
     root_logger.addHandler(SystemdHandler())
 
     server = WebDAVServer(("", port), WebDAVRequestHandler)
