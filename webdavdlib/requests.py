b25da3f59b5362865b6614e3cae06b6fe868cdc6
Daniel Laube <mail@dlaube.de>
2019-03-03 19:38:09 +0100

    implemented requests tests

--- webdavdlib/requests.py
+++ webdavdlib/requests.py
@@ -1,28 +1,33 @@
-from urllib.parse import urlparse
+from urllib.parse import urlparse, unquote
 import base64
 import re
 
 class BaseRequest(object):
     def __init__(self, path, headers, data):
-        self.path = path
+        self.path = unquote(path)
         self.headers = headers
         self.data = data
 
         self.parseDepth()
         self.parseDestination()
         self.parseAuthorization()
+        self.parseLocktoken()
 
     def parseDestination(self):
         self.destination = None
 
         if self.headers.get("Destination"):
-            self.destination = urlparse(self.headers.get("Destination")).path
+            self.destination = unquote(self.headers.get("Destination"))
 
     def parseDepth(self):
         self.depth = None
 
         if self.headers.get("Depth"):
-            self.depth = self.headers.get("Depth")
+            raw = self.headers.get("Depth")
+            if raw.lower() == "infinity":
+                self.depth = 32
+            else:
+                self.depth = int(raw)
 
 
     def parseAuthorization(self):
---
+++ webdavdlib/tests.py
@@ -0,0 +1,49 @@
+import unittest
+import webdavdlib.requests
+
+class RequestParserTest(unittest.TestCase):
+    def testDestination(self):
+        request = webdavdlib.requests.BaseRequest("",  {"Destination" : "/home/test/test.txt"}, "")
+        self.assertEqual(request.destination, "/home/test/test.txt")
+
+        request = webdavdlib.requests.BaseRequest("", {}, "")
+        self.assertEqual(request.destination, None)
+
+    def testDepth(self):
+        request = webdavdlib.requests.BaseRequest("",  {"Depth" : "30"}, "")
+        self.assertEqual(request.depth, 30)
+
+        request = webdavdlib.requests.BaseRequest("", {}, "")
+        self.assertEqual(request.depth, None)
+
+    def testLocktoken(self):
+        request = webdavdlib.requests.BaseRequest("",  {"Lock-Token" : "<opaquelocktoken:testtoken>"}, "")
+        self.assertEqual(request.locktoken, "testtoken")
+
+        request = webdavdlib.requests.BaseRequest("", {"If": "<opaquelocktoken:testtoken>"}, "")
+        self.assertEqual(request.locktoken, "testtoken")
+
+        request = webdavdlib.requests.BaseRequest("", {}, "")
+        self.assertEqual(request.locktoken, None)
+
+    def testAuthorization(self):
+        request = webdavdlib.requests.BaseRequest("",  {"Authorization" : "Basic dGVzdHVzZXI6dGVzdHBhc3N3b3Jk"}, "")
+        self.assertEqual(request.username, "testuser")
+        self.assertEqual(request.password, "testpassword")
+
+        request = webdavdlib.requests.BaseRequest("", {"Authorization": "Basic c29tZXRoaW5nZHVtYg=="}, "")
+        self.assertEqual(request.username, None)
+        self.assertEqual(request.password, None)
+
+        request = webdavdlib.requests.BaseRequest("", {}, "")
+        self.assertEqual(request.depth, None)
+
+    def testPath(self):
+        request = webdavdlib.requests.BaseRequest("/home/test/test.txt", {}, "")
+        self.assertEqual(request.path, "/home/test/test.txt")
+
+        request = webdavdlib.requests.BaseRequest("/home/test/test%20test.txt", {}, "")
+        self.assertEqual(request.path, "/home/test/test test.txt")
+
+if __name__ == "__main__":
+    unittest.main()
